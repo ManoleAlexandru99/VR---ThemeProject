0c84731cd19bd6f2cd5179552f8c9169d8161cfd
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2021-01-09 11:48:31 +0200

    Code and Assets for the first two levels
    Contains player movement, picking objects up, camera post processing + sound when underwater and bouncing object material.

---
+++ Assets/PickUpBall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpBall : MonoBehaviour
+{
+
+    public Transform theDest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("Carry").transform;
+        
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<SphereCollider>().enabled = true;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/UnderWaterSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class UnderWaterSound : MonoBehaviour
+{
+    public AudioSource player;    
+    public AudioSource splash;
+
+    public ParticleSystem particleLauncher;
+    void Start ()
+    {
+    }        
+    
+    void OnTriggerEnter(Collider other)
+    {
+        particleLauncher.transform.position = other.transform.position;
+
+        if (other.CompareTag("Ball"))
+        {
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            
+        }
+
+        if (other.CompareTag("Player") )
+            player.Play ();
+        splash.Play();
+        particleLauncher.Emit(500);
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        player.Stop();
+
+    }
+
+}
